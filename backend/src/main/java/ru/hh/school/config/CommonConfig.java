ffd21b0cd22e852c51943cfc831d61513027bc5a
Ilya Safonov <29283825+Sparkymann@users.noreply.github.com>
2022-04-10 20:08:29 +0300

    add beans in config

--- backend/src/main/java/ru/hh/school/config/CommonConfig.java
+++ backend/src/main/java/ru/hh/school/config/CommonConfig.java
@@ -1,17 +1,36 @@
 package ru.hh.school.config;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import ru.hh.nab.hibernate.MappingConfig;
 import ru.hh.nab.starter.NabCommonConfig;
+import ru.hh.school.dao.AreaDao;
+import ru.hh.school.dao.FavoriteEmployerDao;
+import ru.hh.school.dao.FavoriteVacancyDao;
+import ru.hh.school.resource.EmployerResource;
 import ru.hh.school.resource.ExampleResource;
+import ru.hh.school.resource.FavoriteEmployerResource;
+import ru.hh.school.resource.FavoriteVacancyResource;
+import ru.hh.school.resource.VacancyResource;
+import ru.hh.school.service.FavoriteEmployerService;
+import ru.hh.school.service.FavoriteVacancyService;
 
 @Configuration
 @Import({
-  // import your beans here
-  ExampleResource.class,
-  NabCommonConfig.class
+        // import your beans here
+        EmployerResource.class,
+        VacancyResource.class,
+        FavoriteEmployerResource.class,
+        FavoriteEmployerDao.class,
+        AreaDao.class,
+        FavoriteEmployerService.class,
+        FavoriteVacancyDao.class,
+        FavoriteVacancyService.class,
+        FavoriteVacancyResource.class,
+        ExampleResource.class,
+        NabCommonConfig.class
 })
 public class CommonConfig {
 
@@ -21,4 +40,10 @@ public class CommonConfig {
     mappingConfig.addPackagesToScan("ru.hh.school.entity");
     return mappingConfig;
   }
+
+  @Bean
+  public ModelMapper modelMapper() {
+    return new ModelMapper();
+  }
+
 }
